026a0667d128817837abc7782e50e19ed29d089b
mantse <mmakgaba@gmail.com>
2023-08-28 07:36:32 +0200

    Bumped version number

--- src/dependabot-dep-poc/__init__.py
+++ src/dependabot-dep-poc/__init__.py
@@ -16,7 +16,7 @@ def get_bool_from_envvar(envvar: str) -> bool:
         return False
 
 
-__version__ = "2023.8.4"
+__version__ = "2023.8.5"
 
 VERSION = __version__
 ROOT_DIR = pathlib.Path(__file__).absolute().parent
